8aeb7d4d161509d0184aae4f01694ab50b52bd99
CharlieD5 <charlied5@comcast.net>
2019-02-10 16:11:57 -0800

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Charlie Davidson on 2/10/19.
+//  Copyright © 2019 Charlie Davidson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

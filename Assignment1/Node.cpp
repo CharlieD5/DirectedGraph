7e30a1dc05b6c79b4cd4f7964de291ecb3a4bd2e
CharlieD5 <charlied5@comcast.net>
2019-04-08 21:00:34 -0700

    created assignment1

---
+++ Assignment1/Node.cpp
@@ -0,0 +1,9 @@
+//
+//  Node.cpp
+//  Assignment1
+//
+//  Created by Charlie Davidson on 2/10/19.
+//  Copyright © 2019 Charlie Davidson. All rights reserved.
+//
+
+#include "Node.hpp"
---
+++ Assignment1/Node.hpp
@@ -0,0 +1,14 @@
+//
+//  Node.hpp
+//  Assignment1
+//
+//  Created by Charlie Davidson on 2/10/19.
+//  Copyright © 2019 Charlie Davidson. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+
+#endif /* Node_hpp */
